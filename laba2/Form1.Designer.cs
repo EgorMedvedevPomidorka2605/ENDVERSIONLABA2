bb49e3e77f21ec0f89bb8c33f7ef9b2804034ef6
Egor Medvedev <medvedev.egor.ru@mail.ru>
2021-12-28 14:35:03 +0700

    Добавьте файлы проекта.

---
+++ laba2/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿
+namespace laba2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.стартToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.методToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.назадToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.впередToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.очиститьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lable = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(-2, 32);
+            this.zedGraphControl1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(880, 369);
+            this.zedGraphControl1.TabIndex = 0;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-2, 413);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "x1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(159, 414);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "x2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(320, 414);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "e";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(474, 412);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "F(x)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 409);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(189, 410);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 27);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(343, 409);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(125, 27);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(513, 411);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(125, 27);
+            this.textBox4.TabIndex = 8;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.стартToolStripMenuItem,
+            this.методToolStripMenuItem,
+            this.назадToolStripMenuItem,
+            this.впередToolStripMenuItem,
+            this.очиститьToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(934, 28);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // стартToolStripMenuItem
+            // 
+            this.стартToolStripMenuItem.Name = "стартToolStripMenuItem";
+            this.стартToolStripMenuItem.Size = new System.Drawing.Size(61, 24);
+            this.стартToolStripMenuItem.Text = "Старт";
+            this.стартToolStripMenuItem.Click += new System.EventHandler(this.стартToolStripMenuItem_Click);
+            // 
+            // методToolStripMenuItem
+            // 
+            this.методToolStripMenuItem.Name = "методToolStripMenuItem";
+            this.методToolStripMenuItem.Size = new System.Drawing.Size(67, 24);
+            this.методToolStripMenuItem.Text = "Метод";
+            this.методToolStripMenuItem.Click += new System.EventHandler(this.методToolStripMenuItem_Click);
+            // 
+            // назадToolStripMenuItem
+            // 
+            this.назадToolStripMenuItem.Name = "назадToolStripMenuItem";
+            this.назадToolStripMenuItem.Size = new System.Drawing.Size(65, 24);
+            this.назадToolStripMenuItem.Text = "Назад";
+            this.назадToolStripMenuItem.Click += new System.EventHandler(this.назадToolStripMenuItem_Click);
+            // 
+            // впередToolStripMenuItem
+            // 
+            this.впередToolStripMenuItem.Name = "впередToolStripMenuItem";
+            this.впередToolStripMenuItem.Size = new System.Drawing.Size(74, 24);
+            this.впередToolStripMenuItem.Text = "Вперед";
+            this.впередToolStripMenuItem.Click += new System.EventHandler(this.впередToolStripMenuItem_Click);
+            // 
+            // очиститьToolStripMenuItem
+            // 
+            this.очиститьToolStripMenuItem.Name = "очиститьToolStripMenuItem";
+            this.очиститьToolStripMenuItem.Size = new System.Drawing.Size(87, 24);
+            this.очиститьToolStripMenuItem.Text = "Очистить";
+            this.очиститьToolStripMenuItem.Click += new System.EventHandler(this.очиститьToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(67, 24);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // lable
+            // 
+            this.lable.AutoSize = true;
+            this.lable.Location = new System.Drawing.Point(645, 413);
+            this.lable.Name = "lable";
+            this.lable.Size = new System.Drawing.Size(34, 20);
+            this.lable.TabIndex = 10;
+            this.lable.Text = "min";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(685, 412);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(125, 27);
+            this.textBox5.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 474);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.lable);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem стартToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem методToolStripMenuItem;
+        private System.Windows.Forms.Label lable;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.ToolStripMenuItem назадToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem впередToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem очиститьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+    }
+}
+
---
+++ laba2/Form1.cs
@@ -0,0 +1,235 @@
+﻿using org.mariuszgromada.math.mxparser;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+
+namespace laba2
+{
+    public partial class Form1 : Form
+    {
+        PointPairList list = new PointPairList();
+        PointPairList list1;
+        List<PointPairList> list2 = new List<PointPairList>();
+        LineItem myCurve_1;
+        int index = -1;
+        public Form1()
+        {
+            InitializeComponent();
+            textBox5.ReadOnly = true;
+        }
+        private void стартToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var g = new Task(start);
+            g.Start();
+        }
+        public void start()
+        {
+
+            try
+            {
+                GraphPane pane = zedGraphControl1.GraphPane;
+                pane.CurveList.Clear();
+                string x1 = textBox1.Text;
+                double x_1 = Convert.ToDouble(x1);
+                string x2 = textBox2.Text;
+                double x_2 = Convert.ToDouble(x2);
+                string e1 = textBox3.Text;
+                double e_1 = Convert.ToDouble(e1);
+                string func = textBox4.Text;
+
+                if (e_1 <= 0)
+                {
+                    throw new Exception();
+                }
+                if (x_1 > x_2)
+                {
+                    throw new Exception();
+                }
+                for (double z = x_1; z <= x_2; z += 1)
+                {
+                    Argument x = new Argument("x = " + z);
+                    Expression exp = new Expression(func, x);
+                    if (double.IsNaN(exp.calculate()))
+                    {
+                        throw new Exception();
+                    }
+                    list.Add(z, exp.calculate());
+                    LineItem myCurve = pane.AddCurve("", list, Color.Blue, SymbolType.None);
+                }
+                zedGraphControl1.AxisChange();
+                zedGraphControl1.Invalidate();
+            }
+            catch
+            {
+                MessageBox.Show("Ашибка");
+            }
+        }
+        private void методToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var t = new Task(minf);
+            t.Start();
+        }
+        private void minf()
+        {
+            GraphPane pane = zedGraphControl1.GraphPane;
+            string x1 = textBox1.Text;
+            double x_1 = Convert.ToDouble(x1);
+            string x2 = textBox2.Text;
+            double x_2 = Convert.ToDouble(x2);
+            string e1 = textBox3.Text;
+            double e_1 = Convert.ToDouble(e1);
+            string func = textBox4.Text;
+
+            Argument x;
+            Expression exp;
+            double xx1 = 0;
+            double xx2 = 0;
+            double yy1 = 0;
+            double yy2 = 0;
+            double phi = (Math.Sqrt(5) - 1) / 2;
+            double mmm;
+            while (true)
+            {
+                xx1 = x_2 - (x_2 - x_1) * phi;
+                xx2 = x_1 + (x_2 - x_1) * phi;
+                x = new Argument("x = " + xx1);
+                exp = new Expression(func, x);
+                yy1 = exp.calculate();
+                x = new Argument("x = " + xx2);
+                exp = new Expression(func, x);
+                yy2 = exp.calculate();
+
+                if (yy1 >= yy2)
+                {
+
+                    x_1 = xx1;
+                    x = new Argument("x = " + x_1);
+                    exp = new Expression(func, x);
+                    list1 = new PointPairList();
+                    list1.Add(new PointPair(x_1, exp.calculate()));
+                    list2.Add(list1);
+                    Debug.WriteLine("y1>=y2=>x1=" + x_1);
+                }
+                else
+                {
+                    x_2 = xx2;
+                    x = new Argument("x = " + x_2);
+                    exp = new Expression(func, x);
+                    list1 = new PointPairList();
+                    list1.Add(new PointPair(x_2, exp.calculate()));
+                    list2.Add(list1);
+                    Debug.WriteLine("y1<y2=>x2=" + x_2);
+
+                }
+                if (Math.Abs(x_2 - x_1) < e_1)
+                {
+                    Debug.WriteLine("break: " + yy2 + " " + yy1);
+                    break;
+                }
+            }
+            mmm = (x_1 + x_2) / 2;
+            x = new Argument("x = " + mmm);
+            exp = new Expression(func, x);
+            double yyy = exp.calculate();
+            Action text = () => textBox5.Text = String.Format("{0}, {1}", Math.Round(mmm, textBox3.Text.Length - 2), Math.Round(yyy, textBox3.Text.Length - 2));
+            Invoke(text);
+            list1 = new PointPairList();
+            list1.Add(new PointPair(mmm, yyy));
+            list2.Add(list1);
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            textBox3.MaxLength = 10;
+        }
+        private void назадToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var b = new Task(back);
+            b.Start();
+        }
+        public void back()
+        {
+            GraphPane pane = zedGraphControl1.GraphPane;
+            pane.CurveList.Remove(myCurve_1);
+            if (index > 0)
+            {
+                --index;
+                myCurve_1 = pane.AddCurve("", list2[index], Color.Orange, SymbolType.Circle);
+                zedGraphControl1.Invalidate();
+                Action text = () => textBox5.Text = String.Format("{0}, {1}", Math.Round(list2[index][0].X, textBox3.Text.Length - 2), Math.Round(list2[index][0].Y, textBox3.Text.Length - 2));
+                Invoke(text);
+            }
+        }
+        private void впередToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var f = new Task(forward);
+            f.Start();
+        }
+        public void forward()
+        {
+            GraphPane pane = zedGraphControl1.GraphPane;
+            pane.CurveList.Remove(myCurve_1);
+            if (index < list2.Count - 1)
+            {
+                ++index;
+                myCurve_1 = pane.AddCurve("", list2[index], Color.Orange, SymbolType.Circle);
+                zedGraphControl1.Invalidate();
+                Action text = () => textBox5.Text = String.Format("{0}, {1}", Math.Round(list2[index][0].X, textBox3.Text.Length - 2), Math.Round(list2[index][0].Y, textBox3.Text.Length - 2));
+                Invoke(text);
+            }
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8 && number != 44 && number != 45 && number != 46)
+            {
+                e.Handled = true;
+            }
+        }
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8 && number != 44 && number != 45 && number != 46)
+            {
+                e.Handled = true;
+            }
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.MaxLength = 7;
+        }
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8 && number != 44 && number != 45 && number != 46)
+            {
+                e.Handled = true;
+            }
+        }
+        private void очиститьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GraphPane pane = zedGraphControl1.GraphPane;
+            pane.CurveList.Clear();
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+        }
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
